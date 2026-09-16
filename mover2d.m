e119688b8a8d5903a49285f76c3426471b655b4e
Schwarz, Chris W <chris-schwarz@uiowa.edu>
2026-09-16 11:13:24 -0500

    add a function to project the path out assuming constant velocity. added a ploti function

--- mover2d.m
+++ mover2d.m
@@ -69,6 +69,106 @@ classdef mover2d < box2d
             one = one.update;
         end
         
+        function [obji,distTrav,distProjected] = projectVelocity(obj,Fs,time)
+            % if there are any non-finite values (NaN, Inf) in the object,
+            % it will mess up the interpolation done for distProj below, so
+            % we must fill them in with interpolated values
+            if ~all(isfinite(obj.velocity))
+                isValid = isfinite(obj.velocity);
+                f = (1:length(obj.velocity))';
+                obj.velocity = interp1(f(isValid),obj.velocity(isValid),f,'linear','extrap');
+                obj.velocity(obj.velocity<0) = 0;
+                obj.reference(:,1) = interp1(f(isValid),obj.reference(isValid,1),f,'linear','extrap');
+                obj.reference(:,2) = interp1(f(isValid),obj.reference(isValid,2),f,'linear','extrap');
+                obj.angleDeg = interp1(f(isValid),obj.angleDeg(isValid),f,'linear','extrap');
+                obj = obj.update;
+            end
+            % calculate projection to future distance assuming constant
+            % velocity from its value at each time. results in a projected
+            % distance vector as some future time
+            velocity = obj.velocity;
+            distTrav = cumsum(velocity)/Fs;
+            % if time is a scalar, then use it for all calculations in the
+            % segment, but if it is a vector then it should be the same
+            % length as the segment and possibly different times are
+            % projected for each element of the vector
+            if isscalar(time)
+                distProj = distTrav + time*velocity;
+            elseif length(time)==length(obj.velocity)
+                distProj = distTrav + time.*velocity;
+            else
+                error('dimensions of time argument are not compatible')
+            end
+            % the projected distance is not allowed to extend beyond the
+            % distance travelled because we don't have data out that far
+            isOver = distProj>distTrav(end);
+            isUnder = distProj<distTrav(1);
+            isOut = isOver | isUnder;
+            distProj(isOut) = [];
+            vel = velocity(~isOut);
+            try
+                % use interpolation to map the current distance travelled to
+                % the projected distance travelled
+                refxi = interp1(distTrav,obj.reference(:,1),distProj,'linear','extrap');
+                refyi = interp1(distTrav,obj.reference(:,2),distProj,'linear','extrap');
+                angledegi = interp1(distTrav,obj.angleDeg,distProj,'linear','extrap');
+            catch err
+                % we can fix the error that stems from not having a
+                % monotonic 'xi' variable
+                if ~isempty(strfind(err.message,'monotonic'))
+                    % unique and sorted version of distTrav
+                    [distTravUnique,IAtrav,ICtrav] = unique(distTrav,'stable');
+                    velocity = velocity(IAtrav);
+                    [distTravSort,Is] = unique(distTrav);
+                    % unique and sorted version of distProj
+                    [distProjUnique,IAproj,ICproj] = unique(distProj,'stable');
+                    [distProjSort,Isort] = sort(distProjUnique);
+                    I = (1:length(distProjUnique))';
+                    IunsortProj(Isort) = I; % find the reverse sort indices
+                    % use interpolation to map the current distance travelled to
+                    % the projected distance travelled
+                    refxi = interp1(distTrav(Is),obj.reference(Is,1),distProjSort,'linear','extrap');
+                    refxi = refxi(IunsortProj); % undo sort
+                    refxi = refxi(ICproj); % undo unique
+                    refyi = interp1(distTrav(Is),obj.reference(Is,2),distProjSort,'linear','extrap');
+                    refyi = refyi(IunsortProj); % undo sort
+                    refyi = refyi(ICproj); % undo unique
+                    angledegi = interp1(distTrav(Is),obj.angleDeg(Is),distProjSort,'linear','extrap');
+                    angledegi = angledegi(IunsortProj); % undo sort
+                    angledegi = angledegi(ICproj); % undo unique
+                else
+                    keyboard
+                end
+            end
+            % filter the object variables
+            [B,A] = butter(2,0.5);
+            if length(refxi)>6
+                refxif = filtfilt(B,A,double(refxi));
+                refyif = filtfilt(B,A,double(refyi));
+                angledegif = filtfilt(B,A,double(angledegi));
+            else
+                refxif = refxi;
+                refyif = refyi;
+                angledegif = angledegi;
+            end
+            % build the full variables
+            distProjected = NaN(size(distTrav));
+            distProjected(~isOut) = distProj;
+            velocity = NaN(size(distTrav));
+            velocity(~isOut) = vel;
+            reference = NaN(size(distTrav,1),2);
+            reference(~isOut,:) = [refxif refyif];
+            angleDeg = NaN(size(distTrav));
+            angleDeg(~isOut) = angledegif;
+            % create new object at projected distances
+            obji = mover2d(obj.length,obj.width,obj.refoffset);
+            obji = obji.setreference(reference);
+            obji = obji.setangleDeg(angleDeg);
+            obji = obji.setvelocity(velocity);
+            obji = obji.update;
+        end
+        
+        
         % predict the mover2d's location after some time, where time is
         % interpreted as the parameter in the parametric line equation.
         % this interpretation is true as long as the length of the line is
@@ -87,10 +187,18 @@ classdef mover2d < box2d
             plot(obj.velline,varargin{:})
         end
         
+        % plot n points of a mover2d
         function plotn(obj,n,varargin)
             plotn@box2d(obj,n,varargin{:});
             hold on
             plotn(obj.velline,n,varargin{:})
+        end
+        
+        % plot a custom set of points of a mover2d
+        function ploti(obj,i,varargin)
+            ploti@box2d(obj,i,varargin{:});
+            hold on
+            ploti(obj.velline,i,varargin{:})
         end
     end
 end
